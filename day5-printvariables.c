3b18d87190747342b32841912fd3c653b10db229
Hayrun Nisa <hayrunnisa_birkan24@erdogan.edu.tr>
2025-11-18 18:10:08 +0300

    Add files via upload

---
+++ day5-printvariables.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+   int main()
+
+{
+
+  // student data 
+
+   int studentID = 15;
+   int studentAge = 23;
+   float studentFee = 75.25;
+   char studentGrade = 'B';
+
+   // print variables
+   printf("Student id: %d\n" , studentID);
+   printf("Student age: %d\n" , studentAge);
+   printf("Student fee: %f\n" , studentFee);
+   printf("Student grade: %c\n" , studentGrade);
+
+   return 0;
+
+}  
