fce9737aeaf5fb5ead614bb6751afae14290593d
Hayrun Nisa <hayrunnisa_birkan24@erdogan.edu.tr>
2025-11-19 15:32:27 +0300

    Add files via upload

---
+++ day6-calculate_area.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+ // create integer variables
+ int length = 4;
+ int width = 6;
+ int area;
+
+// calculate area of a rectangle
+ area = length * width;
+
+ // print the variables 
+ printf("Length is: %d\n" , length );
+ printf("Width is : %d\n" , width );
+ printf("Area of the rectangle is : %d" , area );
+
+return 0;
+}
+
