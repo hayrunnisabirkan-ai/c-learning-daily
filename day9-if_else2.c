76d1267d413ab70164b0759acf3844200d0e23a6
Hayrun Nisa <hayrunnisa_birkan24@erdogan.edu.tr>
2025-11-20 14:58:52 +0300

    Add files via upload

---
+++ day9-if_else2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+ int main() {
+
+  int x = 23;
+  int y = 31;
+
+ if(x>y)  
+ 
+ {
+ printf("hayat sevince çok güzel");
+
+ }
+
+ else
+
+ {
+printf ("hayat sevmeyincede çok güzel");  
+ }
+
+     
+ return 0;
+
+ }
