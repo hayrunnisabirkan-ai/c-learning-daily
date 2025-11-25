1887d24761331c85748773a9cb1edbddd52785dd
Hayrun Nisa <hayrunnisa_birkan24@erdogan.edu.tr>
2025-11-25 11:36:03 +0300

    Add files via upload

---
+++ day14-while_rudecu_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*10 dan başlayıp bir azalta azalta 1 e kadar olan sayıları ekrana bastırınız*/
+
+    int i = 10;
+    while (i>0) {
+
+        printf("%d\n",i);
+        i--;
+    }
+    return 0;
+}
