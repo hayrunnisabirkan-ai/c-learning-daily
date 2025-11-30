2496a1138bfa0e7f0d83f8e1188cf8e65860d458
Hayrun Nisa <hayrunnisa_birkan24@erdogan.edu.tr>
2025-11-30 13:04:26 +0300

    Add files via upload

---
+++ day15-coutdown.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int coutdown = 3;
+
+  while (coutdown > 0) {
+    printf("%d\n", coutdown);
+    coutdown--;
+  }
+  printf("Happy New Year!!\n");
+
+  return 0;
+     
+  } 
+
+     
