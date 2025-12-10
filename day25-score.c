0484acb3dff19dead6f40dc4fde7c3f911885678
Hayrun Nisa <hayrunnisa_birkan24@erdogan.edu.tr>
2025-12-10 20:43:54 +0300

    Add files via upload

---
+++ day25-score.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    // set the maximum possible score to 500
+    int maxScore = 500;
+
+    // the actual score of the user
+    int userScore = 423;
+
+    // Calculate the percantage of the user's score in relation to the maximum available score 
+    float percentage = (float) userScore / maxScore * 100.0;
+
+    // Print the percentage
+    printf("User's percentage is %.2f", percentage);
+    
+    return 0;
+    
+}
