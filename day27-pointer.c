d97606dfde482ced0499b645504364eae4d00a7a
Hayrun Nisa <hayrunnisa_birkan24@erdogan.edu.tr>
2025-12-13 21:34:27 +0300

    Add files via upload

---
+++ day27-pointer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()  {
+
+    int can=15;
+    int *p;
+
+    p=&can;
+
+    printf("%d\n", *p);
+     printf("%d\n",p);
+      printf("%d\n",can);
+       printf("%d\n",&can);
+
+return 0;
+ 
+}
