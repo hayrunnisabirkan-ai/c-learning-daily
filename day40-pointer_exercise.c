db6e52072f61b849b92ebebd9003d9888f57ee1c
Hayrun Nisa <hayrunnisa_birkan24@erdogan.edu.tr>
2025-12-31 21:28:25 +0300

    Add files via upload

---
+++ day40-pointer_exercise.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+/*  Farklı data typeler için (char int float double) değişken tanımlayıp 
+değer ataması yapınız hem bu değerleri hem de bellekte tuttukları adresi
+console a  bastırınız. */ 
+
+ char character='C';
+ int integernumber=1;
+ float floatnumber=5.66;
+ double doublenumber=234536600;
+
+ printf("Character = %c, Character of adress =%d\n", character, &character);
+ printf("integernumber = %c, integer of adress =%d\n", character, &character);
+ printf("Character = %c, Character of adress =%d\n", character, &character);
+ printf("Character = %c, Character of adress =%d\n", character, &character);
+}
